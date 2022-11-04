50354db3fccf98b9d37f31478b7ffa6291bb0a80
NitroZ-57 <quentincomte57@gmail.com>
2022-11-04 19:09:34 +0100

    avancée index.php

--- Php/Inclusions/MenuBarre.inc.php
+++
@@ -1,3 +0,0 @@
-<?php
-// Barre de navigation
-?>
--- index.php
+++ index.php
@@ -1,3 +1,52 @@
 <?php
-// Page d'accueil 
+session_start();
+include("Php/Inclusions/Donnees.inc.php");
+
+
 ?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Gestion de Cocktails </title>
+    <meta charset="UTF-8" />
+    <link rel="stylesheet" href="Css/common.css" />
+</head>
+
+<header>
+
+    <form action="<?php $_SERVER['PHP_SELF'] ?>"> 
+        <input type="submit" value="Navigation" name="navigation" />
+        <input type="button" value="Recettes favories" name="recettes favories"/>
+        Recherche : <input type="text" value="" name="recherche" />
+        <input type="submit" value="Valider" name="valider"/>
+    </form>
+
+<?php
+        include("Php/Inclusions/ZoneConnexion.php");
+?>
+
+
+</header>
+
+<nav>
+    <h3> Aliment courant </h3>
+    <!-- TODO -->
+    <h4> Sous-catégories </h4>
+    <!-- TODO -->
+</nav>
+
+
+<main>
+    <h3> Liste des Cocktails </h3>
+    <!-- TODO -->
+</main>
+
+
+
+<footer>
+    <i> Made by Comte Quentin, Perin nicolas & Polkowski romain </i>
+</footer>
+
+</html>
