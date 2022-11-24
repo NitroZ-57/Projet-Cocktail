ed9e0bf552248a0afc21e437807cadce89a7079e
RomainPol <114658777+RomainPol@users.noreply.github.com>
2022-11-24 15:22:00 +0100

    tri des recettes résultats de la recherche
    + tri du code (ajout de fonctions, corrections etc)

--- Inclusions/Recherches.inc.php
+++ Inclusions/Recherches.inc.php
@@ -9,53 +9,10 @@
         ?> <br /><?php
     }
 
-    //procédure qui ajoute la recette si elle contient ingredient (ou un de ses "fils") dans le tableau recettesValides
-    function RecettesContenantIngredient($ingredient, $hierarchie, $recettes, &$recettesValides, &$indiceRecettesValides, $recettesBlacklist, &$ingredientsDejaVus) {
-        retourLigne();
-        retourLigne();
-        echo "ingredient : ".$ingredient;
-        retourLigne();
-        retourLigne();
-        if(!in_array($ingredient, $ingredientsDejaVus)) {
-            $ingredientsDejaVus[] = $ingredient;
-            foreach($recettes as $recette) {
-                if(!in_array($recette, $recettesBlacklist)) {
-                    if(in_array($ingredient, $recette["index"])) {//si l'aliment est ingrédient de la recette
-                        echo $recette["titre"].", ";
-                        
-                        $indiceValide = -1; //la recette est par défaut absente des recettes déjà trouvées (-1 -> absente)
-                        foreach($recettesValides as $indiceRecette => $RecetteValide) {
-                            if($RecetteValide["Cocktail"] == $recette) { //si la recette en question a déjà été trouvée
-                                $indiceValide = $indiceRecette;
-                            }
-                        }
-                        
-                        if($indiceValide == -1) { //si c'est la première fois qu'on trouve la recette
-                            $recettesValides[$indiceRecettesValides]["Cocktail"] = $recette; //on ajoute la recette à la liste de résultats
-                            $recettesValides[$indiceRecettesValides]["nbIngredientsRecherches"] = 1; //on initialise à 1
-                            $indiceRecettesValides++;
-                        }
-                        else {
-                            $recettesValides[$indiceValide]["nbIngredientsRecherches"]++; //sinon on peut accéder au nombre d'ingrédients présents et on incrémente ce nombre
-                        }
-                    }
-                }
-            }
-            if(!empty($hierarchie[$ingredient]['sous-categorie'])) { //si l'aliment a des sous categories, on les évalue
-                foreach($hierarchie[$ingredient]["sous-categorie"] as $sousIngredient) {
-                    RecettesContenantIngredient($sousIngredient, $hierarchie, $recettes, $recettesValides, $indiceRecettesValides, $recettesBlacklist, $ingredientsDejaVus); //on vérifie récursivement si l'aliment est ingrédient de la recette
-                }
-            }
-        }
-    }
-
-//TODO traitement des doublons (avec + et -) en entrée à faire?
-    $finalTab = array();
-    if(isset($_POST['Recherche'])) {
-
+    function TraitementRecherche($recherche) {
         //ÉLIMINATION DES ESPACES, MOTS COMPOSÉS, MOTS VIDES...
+        $finalTab = array();
         $tab = explode(" ",$_POST['Recherche']); //on convertit la recherche en tableau de mots à traiter en séparant par espaces
-        
         
         $quote = false; //boolean qui indique si un mot composé a commencé à être parcouru
         $decalage = 0; //decalage que l'on doit effectuer s'il y a un mot invalide
@@ -82,18 +39,15 @@
                 }
             }
         }
-        
-        //affichage
-        echo "tableau d'elements à traiter : "; print_r($tab); 
-        retourLigne();
-        //affichage
-        echo "tableau d'elements coherents : "; print_r($finalTab);
-        retourLigne();
-        
-        $IngSouhaites = array();
-        $IngNonSouhaites = array();
-        $IngInvalides = array();
-        foreach($finalTab as $ingredient) {
+        if($quote == true) { //si l'on est entré dans un mot composé mais qu'on ne l'a pas fermé
+            echo "Problème de syntaxe dans votre requête : nombre impair de double-quotes";
+        }
+        return $finalTab;
+    }
+
+
+    function verifRecherche($tabRecherche, $Hierarchie, &$IngSouhaites, &$IngNonSouhaites, &$IngInvalides) {
+        foreach($tabRecherche as $ingredient) {
             //ACCES ET VERIFICATION DES ALIMENTS ENTRES DANS LA BARRE DE RECHERCHE
             $ing = trim($ingredient, "-+"); //on récupère le nom de l'ingrédient
             $valide = false; //on part du principe que l'ingrédient n'est pas valide
@@ -115,56 +69,117 @@
                 $IngInvalides[] = $ingredient;
             }
         }
-        retourLigne();
-        //affichage
-        if($quote == true) { //si l'on est entré dans un mot composé mais qu'on ne l'a pas fermé
-            echo "Problème de syntaxe dans votre requête : nombre impair de double-quotes";
-        }
-        else if(empty($IngNonSouhaites) && empty($IngSouhaites)) { //si l'on a pas de critère de recherche valide
+    }
+
+    function AfficherIngResultat($ingsouhaites, $ingnonsouhaites, $inginvalides) {
+        if(empty($ingnonsouhaites) && empty($ingsouhaites)) { //si l'on a pas de critère de recherche valide
             echo "Problème dans votre requête : recherche impossible";
         }
         else {
-            echo "Liste des aliments souhaités : "; foreach($IngSouhaites as $ingredient) echo $ingredient.", ";
+            echo "Liste des aliments souhaités : "; foreach($ingsouhaites as $ingredient) echo $ingredient.", ";
             retourLigne();
-            echo "Liste des aliments non souhaités : "; foreach($IngNonSouhaites as $ingredient) echo $ingredient.", ";
+            echo "Liste des aliments non souhaités : "; foreach($ingnonsouhaites as $ingredient) echo $ingredient.", ";
             retourLigne();
-            echo "Liste des aliments invalides : "; foreach($IngInvalides as $ingredient) echo $ingredient.", "; 
+            echo "Liste des aliments invalides : "; foreach($inginvalides as $ingredient) echo $ingredient.", "; 
         }
-        
-        retourLigne();
-        //TRAITEMENT DES RECETTES
-        echo "Recettes correspondantes : "; //affichage
-        $RecettesValides = array();
-        $RecettesBlacklist = array();
-        $ingredientsDejaVus = array();
-        $indice = 0;
-        $nbIngTotal = count($IngSouhaites);
+    }
 
-        foreach($IngNonSouhaites as $ingredient) { //pour tous les ingrédients non souhaités
-            foreach($Recettes as $recette) {
+    function ajoutIngRecherche($Ingredient, $Hierarchie, &$TabTotalIngs) {
+        if(!empty($Hierarchie[$Ingredient]['sous-categorie'])) { //si l'aliment a des sous categories, on les évalue
+            foreach($Hierarchie[$Ingredient]["sous-categorie"] as $sousIngredient) {
+                ajoutIngRecherche($sousIngredient, $Hierarchie, $TabTotalIngs); //on ajoute récursivement toutes les sous-catégories
+            }
+        }
+        if(!in_array($Ingredient, $TabTotalIngs)) {
+            $TabTotalIngs[] = $Ingredient;
+        }
+    }
+
+    function ensIngRecherche($IngredientsRecherche, $Hierarchie) {
+        $TabTotalIngs = array();
+        foreach($IngredientsRecherche as $ingredient) {
+            ajoutIngRecherche($ingredient, $Hierarchie, $TabTotalIngs);
+        }
+        return $TabTotalIngs;
+    }
+
+    function BlacklisterRecettes($ingredients, $recettes) {
+        $RecettesBlacklist = array();
+        foreach($ingredients as $ingredient) { //pour tous les ingrédients non souhaités
+            foreach($recettes as $recette) {
                 if(in_array($ingredient, $recette["index"])) {//si l'aliment est ingrédient de la recette
                     $RecettesBlacklist[] = $recette; //alors on l'ajoute à la liste des recettes à ne pas afficher en résultat
                 }
             }
         }
+        return $RecettesBlacklist;
+    }
 
-        foreach($IngSouhaites as $ingredient) {
-            retourLigne();
-            retourLigne();
-            RecettesContenantIngredient($ingredient, $Hierarchie, $Recettes, $RecettesValides, $indice, $RecettesBlacklist, $ingredientsDejaVus);
-            retourLigne();
+    //procédure qui ajoute la recette si elle contient ingredient dans le tableau recettesValides
+    function RecettesResultatRecherche($recettes, $recettesBlacklist, $TotalIngredientsSouhaites) {
+        $recettesValides = array();
+        $indiceRecettesValides = 0;
+        foreach($recettes as $recette) {
+            if(!in_array($recette, $recettesBlacklist)) {
+                foreach($recette["index"] as $aliment) {
+                    $nbAliments = count($recette["index"]);
+                    if(in_array($aliment, $TotalIngredientsSouhaites)) {
+                        $indiceValide = -1; //la recette est par défaut absente des recettes déjà trouvées (-1 -> absente)
+                        foreach($recettesValides as $indiceRecette => $RecetteValide) {
+                            if($RecetteValide["Cocktail"] == $recette) { //si la recette en question a déjà été trouvée
+                                $indiceValide = $indiceRecette;
+                            }
+                        }
+                        
+                        if($indiceValide == -1) { //si c'est la première fois qu'on trouve la recette
+                            $recettesValides[$indiceRecettesValides]["Cocktail"] = $recette; //on ajoute la recette à la liste de résultats
+                            $recettesValides[$indiceRecettesValides]["score"] = 1/$nbAliments*100; //on initialise à 1
+                            $indiceRecettesValides++;
+                        }
+                        else {
+                            $recettesValides[$indiceValide]["score"] += 1/$nbAliments*100; //sinon on peut accéder au nombre d'ingrédients présents et on incrémente ce nombre
+                        }
+                    }
+                }
+            }
         }
+        return $recettesValides;
+    }
+
+include "Donnees.inc.php";
+    if(isset($_POST['Recherche'])) {
+        $tabRecherche = TraitementRecherche($_POST['Recherche']); //on récupère les aliments de la recherche
+        
+        //affichage
+        echo "tableau d'elements : "; print_r($tabRecherche);
         retourLigne();
+        
+        $IngSouhaites = array();
+        $IngNonSouhaites = array();
+        $IngInvalides = array();
+
+        verifRecherche($tabRecherche, $Hierarchie, $IngSouhaites, $IngNonSouhaites, $IngInvalides);
+
         retourLigne();
+        //affichage
+        AfficherIngResultat($IngSouhaites, $IngNonSouhaites, $IngInvalides);
+        
+        retourLigne();
+        //TRAITEMENT DES RECETTES
+        
+        $TotalIngredientsSouhaites = ensIngRecherche($IngSouhaites, $Hierarchie);
+        $TotalIngredientsNonSouhaites = ensIngRecherche($IngNonSouhaites, $Hierarchie);
+        $RecettesBlacklist = BlacklisterRecettes($TotalIngredientsNonSouhaites, $Recettes);
+        
+        $RecettesValides = RecettesResultatRecherche($Recettes, $RecettesBlacklist, $TotalIngredientsSouhaites);
+        array_multisort(array_column($RecettesValides, 'score'), SORT_DESC, $RecettesValides); //tri des recettes TODO
+
         //affichage
         foreach($RecettesValides as $Recette) {
             $nbIngRecette = count($Recette["Cocktail"]["index"]);
-            echo $Recette["Cocktail"]["titre"]
-            .", nbIngredientsRecherches :".$Recette["nbIngredientsRecherches"]
-            .", pourcentage de satisfaction : ".number_format(($Recette["nbIngredientsRecherches"] * 100) / $nbIngRecette,2)."%";
+            echo number_format($Recette["score"], 2)."% de satisfaction 
+            <----- ".$Recette["Cocktail"]["titre"];
             retourLigne();
         }
-        //affichage des ingrédients traités
-        print_r($ingredientsDejaVus);
     }
 ?>
